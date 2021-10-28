e99eda32bdaeab928168e86f23ecce22b26bcc32
Howard O'Neil <mingkhoitran1234@gmail.com>
2021-10-28 21:02:46 +0700

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -1,6 +1,8 @@
 hdfs dfs -rm -r /usr/Tran_Minh_Khoi_18520947/homework/out || : && \
-    hadoop jar ./target/hadoop-reducejoin-1.0.jar com.hadoop_dev.App \
-        /usr/Tran_Minh_Khoi_18520947/homework/in/cust.txt \
-        /usr/Tran_Minh_Khoi_18520947/homework/in/trans.txt \
-        /usr/Tran_Minh_Khoi_18520947/homework/out && \
-    hdfs dfs -cat /usr/Tran_Minh_Khoi_18520947/homework/out/part-r-00000
+
+hadoop jar ./target/hadoop-reducejoin-1.0.jar com.hadoop_dev.App \
+    /usr/Tran_Minh_Khoi_18520947/homework/in/cust.txt \
+    /usr/Tran_Minh_Khoi_18520947/homework/in/trans.txt \
+    /usr/Tran_Minh_Khoi_18520947/homework/out && \
+    
+hdfs dfs -cat /usr/Tran_Minh_Khoi_18520947/homework/out/part-r-00000
